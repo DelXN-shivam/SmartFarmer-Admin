d5a33ce5f08a3f624a2551903f4655817d607674
joshi-shounak-delxn <joshishounak.delxn@gmail.com>
2025-08-11 12:02:52 +0530

    changes

--- src/app/admin/dashboard/verifier/all/page.js
+++ src/app/admin/dashboard/verifier/all/page.js
@@ -172,12 +172,12 @@ export default function VerifiersPage() {
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 gap-4">
+      <div className="flex flex-col md:flex-row justify-center items-start md:items-center mb-8 gap-16">
         <h1 className="text-3xl font-bold text-gray-900">Verifier Directory</h1>
 
-        <div className="flex flex-col sm:flex-row gap-4 w-full md:w-auto">
+        <div className="flex flex-col sm:flex-row  w-full md:w-auto">
           {/* Status Filter */}
-          <select
+          {/* <select
             value={statusFilter}
             onChange={(e) => setStatusFilter(e.target.value)}
             className="px-3 py-2 border border-gray-300 rounded-lg"
@@ -186,7 +186,7 @@ export default function VerifiersPage() {
             <option value="pending">Pending</option>
             <option value="approved">Approved</option>
             <option value="rejected">Rejected</option>
-          </select>
+          </select> */}
 
           {/* Search Input */}
           <div className="relative">
